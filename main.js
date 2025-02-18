b099719bb46759626cd6839b4b459dec192587d9
Meli <m.hejranfar@gmail.com>
2025-02-18 21:48:36 +0330

    Header Reveal Animation

--- main.js
+++ main.js
@@ -17,3 +17,32 @@ navLinks.addEventListener("click", (e) => {
     navLinks.classList.remove("open");
     menuBtnIcon.classList.setAttribute("class", "ri-menu-3-line");
 })
+
+
+
+// Header Reveal Animation
+const scrollRevealOption = {
+    distance: "50px",
+    origin: "bottom",
+    duration: 1000,
+};
+
+ScrollReveal().reveal(".header-image img", {
+    duration: 1000
+});
+ScrollReveal().reveal(".header-content h1", {
+    ...scrollRevealOption,
+    delay: 500,
+});
+ScrollReveal().reveal(".header-content .section-description", {
+    ...scrollRevealOption,
+    delay: 1000,
+});
+ScrollReveal().reveal(".header-btn", {
+    ...scrollRevealOption,
+    delay: 1500,
+});
+ScrollReveal().reveal(".header-content .socials", {
+    ...scrollRevealOption,
+    delay: 2000,
+});
